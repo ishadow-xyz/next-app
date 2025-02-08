d4c2e6a4d6fbdad802c5cefa4b4e5dca86765728
iShadow-xyz <iShadow.xyzz@gmail.com>
2025-02-08 16:24:32 +0530

    Update homepage welcome text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-xl font-bold">
         <div className="flex items-center gap-2">
-          <h1 className="text-2xl font-bold">Welcome to the app</h1>
+          <h1 className="text-2xl font-bold">Welcome to the sample nextapp</h1>
         </div>
       </h1>
     </div>
